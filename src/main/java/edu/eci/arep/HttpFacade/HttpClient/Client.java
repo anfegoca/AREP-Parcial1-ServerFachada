d085ef65e56779c25b4bdd625b50cd61eb5f0c52
Andrés González <andres.gonzalez-ca@mail.escuelaing.edu.co>
2020-09-04 18:55:45 -0500

    Despliegue server fachada

--- src/main/java/edu/eci/arep/HttpFacade/HttpClient/Client.java
+++ src/main/java/edu/eci/arep/HttpFacade/HttpClient/Client.java
@@ -1,36 +1,40 @@
 package edu.eci.arep.HttpFacade.HttpClient;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Client {
-    public static void main(String [] args){
-        
-        String url = "https://sheltered-hollows-90641.herokuapp.com/results?number=";
+    public static void main(String[] args) {
+
+        String url = "http://localhost:4567/index?number=123&oper=tan";
         String readLine = null;
         String json = null;
-        try{
-        URL server = new URL(url);
-        HttpURLConnection conection = (HttpURLConnection) server.openConnection();
-        conection.setRequestMethod("GET");
-        
-        System.out.println("HOLA "+conection.getResponseMessage());
-        int responseCode = conection.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader in = new BufferedReader(
-                new InputStreamReader(conection.getInputStream()));
-            StringBuffer response = new StringBuffer();
-            while ((readLine = in .readLine()) != null) {
-                response.append(readLine);
-            } in .close();
-            // print result
-           System.out.println("Respuesta "+ response.toString());
-            //GetAndPost.POSTRequest(response.toString());
-        } else {
-            System.out.println("GET NOT WORKED");
-        }
-        }catch(Exception e){
+        try {
+            URL server = new URL(url);
+            HttpURLConnection conection = (HttpURLConnection) server.openConnection();
+            conection.setRequestMethod("GET");
+
+            System.out.println("HOLA " + conection.getResponseMessage());
+            int responseCode = conection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conection.getInputStream()));
+                StringBuffer response = new StringBuffer();
+                while ((readLine = in.readLine()) != null) {
+                    response.append(readLine);
+                }
+                in.close();
+                
+                System.out.println("Respuesta " + response.toString());
+                
+            } else {
+                System.out.println("GET NOT WORKED");
+            }
+        } catch (Exception e) {
 
         }
 
     }
-    
-    
+
 }
